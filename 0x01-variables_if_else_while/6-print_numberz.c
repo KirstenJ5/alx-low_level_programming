d1f0dd0f7ce6875620ecf5cc6f0cb0a6af505137
KirstenJ5 <juliuschante5@gmail.com>
2023-06-18 14:20:26 -0700

    Fixed 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+~                     
