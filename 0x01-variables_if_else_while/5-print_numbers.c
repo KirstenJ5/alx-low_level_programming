afbcb2d41088698a31f4d385e949580de3578771
KirstenJ5 <juliuschante5@gmail.com>
2023-06-18 14:08:42 -0700

    Fixed 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 10; i <10; i++)
+{
+printf("%d" , i)
+}
+putchar('\n')
+return (0);
+}
