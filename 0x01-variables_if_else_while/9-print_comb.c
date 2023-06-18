92cb05496ad3f23940e9c019712ef3584d07575f
KirstenJ5 <juliuschante5@gmail.com>
2023-06-18 14:57:34 -0700

    Fixed 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n =48; n < 58; n++)
+{
+putchar(n)
+if (n != 57)
+{
+putchar (',');
+putchar (' ');
+}
+putchar("\n");
+return (0);
+}
