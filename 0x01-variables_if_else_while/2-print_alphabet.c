d1c506a37523eb915fda4269d524d79c6f9e3cef
KirstenJ5 <juliuschante5@gmail.com>
2023-06-18 13:14:58 -0700

    Fixed 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet [26] = 'abcdefghijklmnopqrstuvwxyz';
+int i ;
+for (i = 0 : i < 26 : i++)
+{
+putchar (alphabet[i])
+}
+putchar ('\n');
+return (0);
+}
