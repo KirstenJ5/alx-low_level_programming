9c9f7cc1c9bbd87f6af5bfe3edd2b45a4f1785ab
KirstenJ5 <juliuschante5@gmail.com>
2023-06-18 13:32:14 -0700

    Fixed 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+
+for (i = 0 ; i < 52 ; i++)
+{
+putchar (alp[i]);
+}
+putchar ('\n');
+return (0);
+}
