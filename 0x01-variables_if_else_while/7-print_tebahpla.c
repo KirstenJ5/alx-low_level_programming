1c944e7cf1ef6e5a0b92f53a1247c9edf6490c39
KirstenJ5 <juliuschante5@gmail.com>
2023-06-18 14:36:33 -0700

    Fixed 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (rv = 'z'; i >= 'a'; rv--)
+{
+putchar(rv);
+}
+putchar('\n');
+return (0);
+}
