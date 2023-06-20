a81609009ac9efb3b22f4d647874eba6f6ba8f69
KirstenJ5 <juliuschante5@gmail.com>
2023-06-20 14:11:08 -0700

    Fixed 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	int i;
 
 	for (i = 97; i < 123; i++)
 	{
-		putchar(i);
+		_putchar(i);
 	}
-putchar('\n');
-return (0);
+_putchar('\n');
+
 }
