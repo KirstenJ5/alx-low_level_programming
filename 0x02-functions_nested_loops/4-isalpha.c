b2bd55029a18686db0a75c0333a50d30b1190887
KirstenJ5 <juliuschante5@gmail.com>
2023-06-21 15:07:37 -0700

    Fixed 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,20 +3,16 @@
 /**
  * _isalpha - Checks for alphabetic character
  *
+ * @c:
+ *
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
-	if (c >= 97; c <= 122)
-	{
-		if (c >= 65; c <= 90)
+	if ((c >= 97 &&  c <= 122) || (c >= 65 &&  c <= 90))
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
-	}	
-}
+	return (0);
 
+}
