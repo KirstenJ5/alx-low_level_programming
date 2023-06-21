061681079dfdeeec2c0b4956cb03b1f7c00129b8
KirstenJ5 <juliuschante5@gmail.com>
2023-06-21 13:53:12 -0700

    Fixed 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * _isalpha - Checks for alphabetic character
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 97; c <= 122 | c >= 65; c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
