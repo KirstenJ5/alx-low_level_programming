84249e9e8ce9a40fa343e4d2f20b689bad2752f4
KirstenJ5 <juliuschante5@gmail.com>
2023-06-21 16:02:44 -0700

    Fixed 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+* _abs - Compute the absolute value of an integer
+*
+* @b:
+*
+* Return: Always 0 (Success)
+*/
+int _abs(int b)
+{
+	if (b < 0)
+	{
+		b = -1 * (b % 10);
+		return (b);
+	}
+	return (b % 10);
+}
