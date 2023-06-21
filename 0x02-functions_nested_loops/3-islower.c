7fc4360517b4d17940b7d387fbc8b7f54d56ded5
KirstenJ5 <juliuschante5@gmail.com>
2023-06-21 12:48:17 -0700

    Fixed 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /* betty style doc for function main goes there */
 /**
- * int_islower int c - Check lowercase character
+ * int_islower_int c - Check lowercase character
  *
  * Return: Always 0 (Success)
  */
