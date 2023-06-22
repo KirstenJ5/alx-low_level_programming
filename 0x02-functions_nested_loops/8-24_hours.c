4870ab4cafd5bdbcf7757ad4762e3c653c5cc929
KirstenJ5 <juliuschante5@gmail.com>
2023-06-22 00:20:21 -0700

    Fixed 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include “main.h"
+/* betty style doc for function main goes there */
+/**
+*jack_bauer - Print every minute of the day
+*
+*
+*
+* Return: Always 0 (Success)
+*/
+void jack_bauer(void)
+{
+	int i;
+	int a;
+
+	for ( i = 48; i <= 50; i++)
+	{
+		for (a = 48 ;a < 57; a++)
+		{
+			_putchar(i);
+			_putchar(a);
+			_putchar(':');
+		}
+	}
+}
